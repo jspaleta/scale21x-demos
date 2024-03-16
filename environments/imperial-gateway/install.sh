f5e477b98ea5176e9e964794a374e69c10ca9a73
Jef Spaleta <jspaleta@gmail.com>
2024-03-16 00:38:03 -0700

    finish refactor

--- environments/imperial-gateway/install.sh
+++ environments/imperial-gateway/install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 CILIUM_VERSION="${CILIUM_VERSION:-v1.15.1}"
-### https://github.com/thebsdbox/cilium-grafana-observability-demo/tree/main
 
-
-### Infrascture Admin: provisions the cluster so it supports ingress/gateway-api
+## Create the Kind Cluster
 kind create cluster --config ./kind/kind-config.yaml
 ret=$?
 if [ $ret -ne 0 ]
@@ -11,10 +9,11 @@ then
     kubectl config use-context kind-imperial-gateway
 fi
 
-CDIR=$(pwd)
-cd ../../starwars-docker/
-IMAGE=local/starwars:0.1 make build
-cd $CDIR
+# skip for live demo: use existing local image build
+#CDIR=$(pwd)
+#cd ../../starwars-docker/
+#IMAGE=local/starwars:0.1 make build
+#cd $CDIR
 
 docker pull quay.io/cilium/cilium:${CILIUM_VERSION}
 docker pull quay.io/cilium/operator-generic:${CILIUM_VERSION}
@@ -27,11 +26,13 @@ kind load docker-image local/starwars:0.1 -n imperial-gateway
 kind load docker-image quay.io/cilium/cilium:${CILIUM_VERSION} -n imperial-gateway
 kind load docker-image quay.io/cilium/operator-generic:${CILIUM_VERSION} -n imperial-gateway
 
+## Configure kube-vip to support LoadBalancer IP address assignment
 kubectl apply -f k8s/kube-vip/kube-vip-rbac.yaml
 kubectl create configmap --namespace kube-system kubevip --from-literal range-global=172.18.200.100-172.18.200.200
 kubectl apply -f k8s/kube-vip/kube-vip-cloud-controller.yaml
 docker run --network host --rm ghcr.io/kube-vip/kube-vip:v0.5.7 manifest daemonset --services --inCluster --arp --interface eth0 | kubectl apply -f -
 
+## Install the Gateway API CRDS
 kubectl apply -f k8s/gateway-api/gateway.networking.k8s.io_gatewayclasses.yaml
 kubectl apply -f k8s/gateway-api/gateway.networking.k8s.io_gateways.yaml
 kubectl apply -f k8s/gateway-api/gateway.networking.k8s.io_grpcroutes.yaml
@@ -39,6 +40,5 @@ kubectl apply -f k8s/gateway-api/gateway.networking.k8s.io_httproutes.yaml
 kubectl apply -f k8s/gateway-api/gateway.networking.k8s.io_referencegrants.yaml
 kubectl apply -f k8s/gateway-api/gateway.networking.k8s.io_tlsroutes.yaml
 
-
-
+## Install Cilium
 cilium install --version $CILIUM_VERSION -f ./cilium/helm-values-ingress.yaml 
--- environments/imperial-gateway/seed.sh
+++ environments/imperial-gateway/seed.sh
@@ -4,9 +4,12 @@ kubectl apply -f k8s/empire.yaml
 
 ### Cluster Operator controls setting up the ingress or gateway-api
 kubectl apply -f k8s/deathstar-ingress.yaml
-kubectl apply -f k8s/deathstar-gateway.yaml
 kubectl apply -f k8s/eye-of-palpatine-ingress.yaml
+sleep 10
+kubectl apply -f k8s/deathstar-gateway.yaml
+sleep 10
 kubectl apply -f k8s/simple-infra-gateway.yaml
+
 echo 
 echo "################################################"
 echo "Waiting for cluster to be ready for gateway demo"
@@ -19,10 +22,16 @@ kubectl get gateway -A
 echo "LoadBalancer services:" 
 kubectl get svc -A |grep LoadBalancer
 echo ""
+echo "Checking Deathstar Gateway Setup"
+echo "Deathstar gateway:"
+kubectl get gateway -A |grep deathstar
+echo "Deathstar Gateway DNS name:"
+grep 'www.deathstar' /etc/hosts
+echo ""
 echo "Checking Infra Gateway Setup"
 echo "Infra gateway:"
 kubectl get gateway -A |grep infra
-echo "DNS names:"
+echo "Infra Gateway DNS names:"
 grep infra /etc/hosts
 ### Teams controls the gateway routes
 
